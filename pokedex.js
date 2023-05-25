fea1bde5344527cf2980dae27b2825c9f91e0d89
Daniela Lopez <daniela.lopez@bootcamp-upgrade.com>
2023-05-25 17:59:59 +0200

    pokedex2

--- pokedex.js
+++ pokedex.js
@@ -1,21 +1,27 @@
-async function getPokemons (){
-    const res = await fetch ('https://pokeapi.co/api/v2/pokemon');
-    const respokemons = await res.json();
-    getEachPokemon (respokemons.results);
+async function getPokemons(){
+    const res = await fetch("https://pokeapi.co/api/v2/pokemon")
+    const resPokemons = await res.json();
+    getDetailPokemons(resPokemons.results);
+  }
+  async function getDetailPokemons(pokemons){
+    const pokemonsPromises = pokemons.map(pokemon => fetch(pokemon.url).then(res => res.json()))
+    const detailPokemons = await Promise.all(pokemonsPromises);
+    console.log(detailPokemons)
+  }
 
-   async function getEachPokemon (pokemons) {
-        const pokemonsPromises = []
-        for (const pokemon of pokemons) {
-            pokemonsPromises.push(fetch(pokemon.results))
-        }
-        const resDetailPokemons = await Promise.all(pokemonsPromises);
-        const detailPokemons = []
-        for (const resDetailPokemon of resDetailPokemons) {
-            detailPokemons.push(await resDetailPokemon.json())
-        }
-        console.log(resDetailPokemons);
+  const pokemon = results.map((result) => {
+    name: result.name;
+    image: result.sprites['front_default'];
+    type: result.types.map((type) => type.type.name).join(', ');
+    id: result.id})
 
-    }
+ // function printcharacters (pokemons) {
+ //   for (const pokemon of pokemons) {
+ //   const container$$ = document.createElement('div')
 
-}
+ //   container$$.innerHTML= `
+    
+  // `
+
+
 getPokemons()
