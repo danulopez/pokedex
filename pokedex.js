b996018dc17b624f11cc558beef9d4fee1bfe1d4
Daniela Lopez <daniela.lopez@bootcamp-upgrade.com>
2023-05-27 18:56:16 +0200

    modific2

--- pokedex.js
+++ pokedex.js
@@ -1,27 +1,46 @@
-async function getPokemons(){
-    const res = await fetch("https://pokeapi.co/api/v2/pokemon")
-    const resPokemons = await res.json();
-    getDetailPokemons(resPokemons.results);
-  }
-  async function getDetailPokemons(pokemons){
-    const pokemonsPromises = pokemons.map(pokemon => fetch(pokemon.url).then(res => res.json()))
-    const detailPokemons = await Promise.all(pokemonsPromises);
-    console.log(detailPokemons)
-  }
+let results = [];
 
-  const pokemon = results.map((result) => {
-    name: result.name;
-    image: result.sprites['front_default'];
-    type: result.types.map((type) => type.type.name).join(', ');
-    id: result.id})
+async function getPokemons() {
+  const resPokemons = await fetch("https://pokeapi.co/api/v2/pokemon/?limit='+numPokemon");
+  const data = await resPokemons.json();
+  results = data.results;
+  getDetailPokemons(results);
+}
 
- // function printcharacters (pokemons) {
- //   for (const pokemon of pokemons) {
- //   const container$$ = document.createElement('div')
+async function getDetailPokemons(pokemons) {
+  const pokemonsPromises = pokemons.map(pokemon =>
+    fetch(pokemon.url).then(res => res.json())
+  );
+  const detailPokemons = await Promise.all(pokemonsPromises);
+  createPokemoncard(detailPokemons);
+}
 
- //   container$$.innerHTML= `
+function createPokemoncard(pokemons) {
+  const pokemonContainer = document.getElementById("pokemon-container");
+  
+  pokemons.forEach(result => {
+    const pokemonCard = document.createElement("div");
+    pokemonCard.classList.add("pokemon-card");
     
-  // `
+    const name = document.createElement("h3");
+    name.textContent = result.name;
+    
+    const image = document.createElement("img");
+    image.src = result.sprites?.front_default;
+    
+    const type = document.createElement("p");
+    type.textContent = result.types.map(type => type.type.name).join(", ");
+    
+    const id = document.createElement("p");
+    id.textContent = result.id;
+    
+    pokemonCard.appendChild(name);
+    pokemonCard.appendChild(image);
+    pokemonCard.appendChild(type);
+    pokemonCard.appendChild(id);
+    
+    pokemonContainer.appendChild(pokemonCard);
+  });
+}
 
-
-getPokemons()
+getPokemons();
